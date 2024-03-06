1761d17765799f79f3e02bd74056c91b58b75225
Dwilford1305 <wilfordderek@gmail.com>
2024-03-06 14:42:28 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const hbs = require('hbs');
+const collection = require('./mongodb.js');
+
+const templatePath = path.join(__dirname, '../templates');
+
+app.use(express.json());
+app.set('view engine', 'hbs');
+app.set("views", templatePath);
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.render("login.hbs")
+});
+
+app.get('/signup', (req, res) => {
+    res.render("signup.hbs")
+});
+
+app.post('/login', async (req, res) => {
+try{
+    const check = await collection.findOne({username: req.body.username});
+
+    if(check.password === req.body.password){
+        res.render("home");
+    } else {
+        res.send("Wrong password");
+        res.render("login.hbs");
+        
+    }
+} catch (e) {
+    res.render("login.hbs");
+    res.send("Username not found");
+}
+});
+
+app.post('/signup', async (req, res) => {
+
+    const data = {
+        username: req.body.username,
+        password: req.body.password
+    }
+
+    await collection.insertMany([data]);
+
+    res.render("home")
+
+});
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
---
+++ src/mongodb.js
@@ -0,0 +1,29 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const mongoose = require('mongoose');
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ynuwarp.mongodb.net/loginSignupdb`)
+.then(()=>{
+    console.log("MongoDB connection successful");
+})
+.catch((error)=>{
+    console.log("MongoDB connection FAILED");
+})
+
+const logInSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+
+});
+
+
+const collection = new mongoose.model("users", logInSchema);
+
+module.exports = collection;
