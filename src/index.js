5921eac50b748f3ab9e4ec4dde732788f7db25e5
Dwilford1305 <wilfordderek@gmail.com>
2024-03-07 09:38:49 -0700

    Add bcrypt for password hashing and validation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const hbs = require('hbs');
+const bcrypt = require('bcrypt');
 const collection = require('./mongodb.js');
 
 const templatePath = path.join(__dirname, '../templates');
@@ -31,17 +32,21 @@ app.get('/signup', (req, res) => {
 app.post('/login', async (req, res) => {
 try{
     const check = await collection.findOne({username: req.body.username});
-
-    if(check.password === req.body.password){
-        res.render("home");
-    } else {
-        res.send("Wrong password");
+    if(!check){
+        res.send("Username not found");
         res.render("login.hbs");
-        
+    } else {
+        const validPass = await bcrypt.compare(req.body.password, check.password);
+        if(validPass){
+            res.render("home.hbs");
+        } else {
+            res.send("Invalid Password");
+            res.render("login.hbs");
+        }
+    
     }
-} catch (e) {
-    res.render("login.hbs");
-    res.send("Username not found");
+} catch  {
+    res.send("Wrong Username or Password");
 }
 });
 
@@ -52,9 +57,19 @@ app.post('/signup', async (req, res) => {
         password: req.body.password
     }
 
-    await collection.insertMany([data]);
+    // check if username already exists
+    const existingUser = await collection.findOne({username: data.username});
+    if (existingUser) {
+        res.send("Username already exists");
+    } else {
+        // hash the password using bcrypt
+        const saltRounds = 10;
+        const hashedPass = await bcrypt.hash(data.password, saltRounds);
+        data.password = hashedPass; // replace the password with the hashed password
+        await collection.insertMany([data]);
 
-    res.render("home")
+        res.render("home")
+    }
 
 });
 
