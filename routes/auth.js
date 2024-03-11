070fd5ef48c9f64ce167113e1fa386abdeb9fc4f
Dwilford1305 <wilfordderek@gmail.com>
2024-03-11 16:19:27 -0600

    Update MongoDB connection and API routes

---
+++ routes/auth.js
@@ -0,0 +1,58 @@
+const router = require('express').Router();
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
+
+
+router.get('/', (req, res) => {
+    res.render("signup.hbs")
+});
+
+//SIGNUP
+router.post("/signup", async (req, res) => {
+    const check = await User.findOne({username: req.body.username});
+
+    if (check) {
+        res.send("Username already exists");
+    } else {
+        try {
+            //hash password
+            const salt = await bcrypt.genSalt(10);
+            const hashedPass = await bcrypt.hash(req.body.password, salt);
+
+            //create new user
+            const user = await new User({
+                username: req.body.username,
+                email: req.body.email,
+                password: hashedPass
+            })
+            //save user
+            await user.save();
+            res.redirect("/api/user");
+        } catch (error) {
+            console.log(error);
+        }
+    }
+});
+
+//LOGIN
+router.post('/login', async (req, res) => {
+    try{
+        const check = await User.findOne({username: req.body.username});
+        if (!check) {
+            res.send("Username not found");
+        } else {
+            const validPass = await bcrypt.compare(req.body.password, check.password);
+            if (validPass) {
+                res.redirect("/api/user");
+            } else {
+                res.send("Invalid password");
+            }
+        }
+    } catch (error) {
+        console.log(error);
+    }
+    });
+
+
+module.exports = router;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,27 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const hbs = require('hbs');
-const collection = require('./mongodb.js');
-const bcrypt = require('bcrypt');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+dotenv.config();
+const mongoose = require('mongoose');
+
+mongoose.connect(`${process.env.MONGO_URL}`)
+.then(()=>{
+    console.log("MongoDB connection successful");
+})
+.catch((error)=>{
+    console.log("MongoDB connection FAILED");
+})
 
 const templatePath = path.join(__dirname, '../templates');
 
 app.use(express.json());
 app.set('view engine', 'hbs');
 app.set("views", templatePath);
+app.use(morgan('dev'));
+const userRouter = require('../routes/users.js');
+const authRouter = require('../routes/auth.js');
 app.use(express.urlencoded({ extended: false }));
 
 //import bootstrap
@@ -21,55 +34,17 @@ app.use('/assets/vendor/bootstrap/js',
 app.use('/assets/vendor/bootswatch/dist/vapor', 
     express.static(path.join('node_modules', 'bootswatch', 'dist', 'vapor')));
 
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
 app.get('/', (req, res) => {
     res.render("login.hbs")
 });
-
-app.get('/signup', (req, res) => {
-    res.render("signup.hbs")
-});
-
-app.post('/login', async (req, res) => {
-try{
-    const check = await collection.findOne({username: req.body.username});
-    if (!check) {
-        res.send("Username not found");
-    } else {
-        const validPass = await bcrypt.compare(req.body.password, check.password);
-        if (validPass) {
-            res.render("home");
-        } else {
-            res.send("Invalid password");
-        }
-    }
-} catch (error) {
-    res.status(500).send("Error", error);
-}
-});
-
-app.post('/signup', async (req, res) => {
-    const data = {
-        
-            username: req.body.username,
-            password: req.body.password
-        
-    }
-
-    //check if username already exists
-    const existingUser = await collection.findOne({username: data.username});
-    if(existingUser){
-        res.send("Username already exists");
-    } else {
-    // hash the password
-    const saltRounds = 10;
-    const hashedPass = await bcrypt.hash(data.password, saltRounds);
-    data.password = hashedPass;
-    await collection.insertMany([data]);
-
-    res.render("home")
-    }
-});
+// Moved to routes/users.js
+//app.get('/user', (req, res) => {
+//    res.render("home.hbs")
+//});
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    console.log('Backend server is running on port 3000');
 });
