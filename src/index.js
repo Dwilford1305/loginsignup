3c23f2722a998be5094dd829ecadb20a0c6a986d
Dwilford1305 <wilfordderek@gmail.com>
2024-03-13 14:48:29 -0600

    Update Bootstrap theme to "lux" and make minor UI changes

--- src/index.js
+++ src/index.js
@@ -70,11 +70,11 @@ app.use(session({
 
 app.use(express.static(path.join('public')));
 
-app.use(express.static(path.join('node_modules/bootswatch/dist/morph')));
+app.use(express.static(path.join('node_modules/bootswatch/dist/lux')));
 app.use('/assets/vendor/bootstrap/js', 
     express.static(path.join('node_modules', 'bootstrap', 'dist', 'js')));
-app.use('/assets/vendor/bootswatch/dist/morph', 
-    express.static(path.join('node_modules', 'bootswatch', 'dist', 'morph')));
+app.use('/assets/vendor/bootswatch/dist/lux', 
+    express.static(path.join('node_modules', 'bootswatch', 'dist', 'lux')));
     app.use('/socket.io', express.static(path.join(__dirname, 
         'node_modules', 'socket.io', 'client-dist')));
 app.use("/api/user", userRouter);
